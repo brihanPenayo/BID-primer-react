3877e1a31a543ee6482b3056d86bcea3d9bffb46
Brihan Penayo <brihan.penayos@gmail.com>
2023-01-14 15:33:54 -0300

    Creando app "Apoyarlo"

---
+++ apoyarlo/src/App.js
@@ -0,0 +1,14 @@
+import PersonCard from './components/PersonCard';
+import './index.css';
+
+
+function App() {
+  return (
+    <>
+      <PersonCard />
+      <h1>Hola bro</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ apoyarlo/src/components/PersonCard.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class PersonCard extends Component {
+    render() {
+        return (
+            <h1> Hola Grupo</h1>
+        )
+    }
+}
+
+export default PersonCard;
